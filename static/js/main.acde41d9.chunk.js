781bbd42c80f91e9b19bfa37654b529e2ce4cd8b
Lawrence Fiesta <lfiesta86@gmail.com>
2019-08-02 10:21:02 -0700

    Updates

---
+++ static/js/main.acde41d9.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{3:function(n,e,t){n.exports=t(9)},9:function(n,e,t){"use strict";t.r(e);var r=t(0),o=t.n(r),a=t(2);var c=function(){return o.a.createElement("p",null,"lmn news")};t.n(a).a.render(o.a.createElement(c,null),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.acde41d9.chunk.js.map
