7c8a8184a180d502fca20645457ee52b63eaf4de
Lawrence Fiesta <lfiesta86@gmail.com>
2019-08-07 12:53:03 -0700

    updating

--- lmn/src/pages/Populate/index.js
+++ lmn/src/pages/Populate/index.js
@@ -1,19 +1,43 @@
 import React, { Component } from "react";
 import API from "../../utils/API";
-
+import ArticleContainer from "../../components/ArticleContainer";
+import Wrapper from "../../components/Wrapper";
 class Populate extends Component {
     state = {
-        title: "",
-        link: "",
-        summary: "",
+        articles: []
     };
 
     componentDidMount() {
         this.loadArticles();
+        this.fetchArticles();
     }
 
     loadArticles = () => {
-        // API.scrapeArticles().then(res => "scrape completed").catc
+        API.scrapeArticles().then()
+        .catch(err => console.log(err))
+    };
+
+    fetchArticles = () => {
+        API.getArticles().then(res =>
+            this.setState({
+                articles: res.data
+            }))
+        .catch(err => console.log(err))
+    }
+
+    render() {
+        return (
+            <Wrapper>
+            {this.state.articles.map(articlesObj => (
+                <ArticleContainer
+                    key={articlesObj.id}
+                    title={articlesObj.title}
+                    link={articlesObj.link}
+                    summary={articlesObj.summary}
+                />
+            ))}
+            </Wrapper>
+        );
     }
 }
 
--- lmn/src/utils/API.js
+++ lmn/src/utils/API.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 export default {
   scrapeArticles: function() {
-    return axios.get("https://lmn-news-api.herokuapp.com/scrape/politics")
+    return axios.get("https://lmn-news-api.herokuapp.com/scrape/politics");
   },
   getArticles: function() {
-    return axios.get("https.lmn-news-api.herokuapp.com/api/politics")
+    return axios.get("https://lmn-news-api.herokuapp.com/api/politics");
   }
 
 }
