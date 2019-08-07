8705e787d67e96b2588bc45750fc7cb7e66194cd
ninateng <niinateng@gmail.com>
2019-08-07 10:52:34 -0700

    completed ui for populated page

--- lmn/src/pages/Populate/index.js
+++ lmn/src/pages/Populate/index.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-import React from "react";
-import './style.css';
 
-function Populate () {
-    return (
-        <div>
-            <h1>Populated news</h1>
-        </div>
-    );
-=======
 import React, { Component } from "react";
 import API from "../../utils/API";
+import './style.css';
 
 class Populate extends Component {
     state = {
@@ -26,7 +17,47 @@ class Populate extends Component {
     loadArticles = () => {
         // API.scrapeArticles().then(res => "scrape completed").catc
     }
->>>>>>> 2cc9c5f057444615c27ca8ed6967cbf26c8e5867
+
+    render() {
+        return (
+            <div id="rightBlock">
+            <h1 id="titleFav">Discover</h1>
+            
+
+            {/* -------------------ROW 1---------------- */}
+            <div id="containerPopulated">
+                <div class="column" id="containerYoutube">
+                    <h1>youtube</h1>
+                    <div id="youtube"></div>
+                    <br/>
+                    <br/>
+                    <br/>
+                </div>
+                    
+                <div class="column" id="containerNews">
+                    <h1>news</h1>
+                    <div id="news"></div>
+                    <br/>
+                    <br/>
+                    <br/>
+                </div>
+
+                <div class="column" id="containerPodcast">
+                    <h1>podcast</h1>
+                    <div id="podcast"></div>
+                    <br/>
+                    <br/>
+                    <br/>
+                </div>
+
+            </div>
+            {/* -------------------ROW 1---------------- */}
+
+  
+         </div>
+         
+        );
+    }
 }
 
 export default Populate;
