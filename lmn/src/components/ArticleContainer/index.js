b29449073332f6ac3b8e2b1eb8b177ebbd3eedb5
Meghan Mullally <ms.mullally27@gmail.com>
2019-08-08 11:34:07 -0700

    different card container styling for news container

--- lmn/src/components/ArticleContainer/index.js
+++ lmn/src/components/ArticleContainer/index.js
@@ -1,20 +1,18 @@
 import React from 'react'
+import "./style.css";
 
 export default function ArticleContainer(props) {
   return (
     <div className="card">
       <div className="content">
-        <ul>
-          <li>
-            <strong>Title:</strong> {props.title}
-          </li>
-          <li>
-            <strong>Link:</strong> {props.link}
-          </li>
-          <li>
-            <strong>Summary:</strong> {props.summary}
-          </li>
-        </ul>
+        <div className="card-header">  
+        <h6 className="card-title">{props.title}</h6>
+        </div>
+        <div className="card-body">
+      <p className="card-text">Summary: {props.summary}</p>
+        <a href={props.link} className="btn btn-primary" id="podcast-btn">"{props.title}"</a>
+          </div>
+      
       </div>
     </div>
   )
