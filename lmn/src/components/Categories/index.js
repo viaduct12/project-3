ab3f84d03c36ecfa0483f113389480aa6d9375fa
Meghan Mullally <ms.mullally27@gmail.com>
2019-08-12 14:57:22 -0700

    added Button, Categories, Disucssions, Input, Modal compoenets folder for forum

---
+++ lmn/src/components/Categories/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './style.css';
+
+export default function Categories () {
+
+  return (
+
+  <div>
+       {/* Categories Colum */}
+      <div className="col s4" id="category-container">
+          <div className="card blue-grey darken-1">
+            <div className="card-content white-text">
+              <span className="card-title">Categories</span>
+              <Link to="/forum/announcements"><h6>Announcements</h6></Link>
+            <div className="divider"></div>
+              <Link to="/forum/anime_gaming"><h6>Anime & Gaming</h6></Link>
+              <div className="divider"></div>
+              <Link to="/forum/charities"><h6>Charities</h6></Link>
+              <div className="divider"></div>
+              <Link to="/forum/design"><h6>Design</h6></Link>
+              <div className="divider"></div>
+              <Link to="/forum/environment"><h6>Environment</h6></Link>
+              <div className="divider"></div>
+              <Link to="/forum/media"><h6>Media</h6></Link>
+              <div className="divider"></div>
+              <Link to="/forum/movements"><h6>Movements</h6></Link>
+              <div className="divider"></div>
+              <Link to="/forum/politics"><h6>Politics</h6></Link>
+              <div className="divider"></div>
+              <Link to="/forum/podcasts"><h6>Podcasts</h6></Link>
+              <div className="divider"></div>
+              <Link to="/forum/sports"><h6>Sports</h6></Link>
+              <div className="divider"></div>
+              <Link to="/forum/technology"><h6>Technology</h6></Link>
+              </div>
+            </div>
+          </div>
+  </div>
+
+  )
+  
+
+}
---
+++ lmn/src/components/Modal/index.js
@@ -0,0 +1,174 @@
+import React, { Component } from "react";
+import M from "materialize-css";
+import "materialize-css/dist/css/materialize.min.css";
+import './style.css';
+import Input from '../Input';  
+import TextArea from '../TextArea'; 
+import Button from '../Button';
+
+
+class Modal extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      newPost: {
+        username: '',
+        title: '',
+        text: '',
+        tags: '',
+        category: []
+      },
+
+      categoryOptions: ['Anime & Gaming', 'Charities', 'Design', 'Environment', 'Media', 'Movements', 'Politics', 'Podcasts', 'Sports', 'Technology']
+    }
+
+    this.handleTextArea = this.handleTextArea.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+
+  }
+
+  componentDidMount() {
+    const options = {
+      onOpenStart: () => {
+        console.log("Open Start");
+      },
+      onOpenEnd: () => {
+        console.log("Open End");
+      },
+      onCloseStart: () => {
+        console.log("Close Start");
+      },
+      onCloseEnd: () => {
+        console.log("Close End");
+      },
+      inDuration: 250,
+      outDuration: 250,
+      opacity: 0.5,
+      dismissible: false,
+      startingTop: "4%",
+      endingTop: "10%"
+    };
+    M.Modal.init(this.Modal, options);
+
+    // let instance = M.Modal.getInstance(this.Modal);
+    // instance.open();
+    // instance.close();
+    // instance.destroy();
+  }
+
+handleInput(e) {
+      let value = e.target.value;
+      let name = e.target.name;
+  this.setState( prevState => ({ newPost : 
+        {...prevState.newPost, [name]: value
+        }
+      }), () => console.log(this.state.newPost))
+  }
+
+  handleTextArea(e) {
+    console.log("Inside handleTextArea");
+    let value = e.target.value;
+    this.setState(prevState => ({
+      newPost: {
+        ...prevState.newPost, about: value
+      }
+      }), ()=>console.log(this.state.newPost))
+  }
+
+  // handleFormSubmit(e) {
+  //   e.preventDefault();
+  //   let postData = this.state.newPost;
+
+  //   // fetch('http://example.com',{
+  //   //     method: "POST",
+  //   //     body: JSON.stringify(postData),
+  //   //     headers: {
+  //   //       'Accept': 'application/json',
+  //   //       'Content-Type': 'application/json'
+  //   //     },
+  //   //   })
+  //     .then(response => {
+  //       response.json().then(data =>{
+  //         console.log("Successful" + data);
+  //       })
+  //   })
+  // } 
+
+  handleClearForm(e) {
+  
+    e.preventDefault();
+    this.setState({ 
+      newPost: {
+        username: '',
+        title: '',
+        text: '',
+        tags: '',
+        category: []
+      },
+    })
+}
+
+
+  render() {
+    return (
+      <div>
+        <button
+          className="waves-effect waves-light btn modal-trigger"
+          data-target="modal1"
+        >Create a Post</button>
+
+        <div
+          ref={Modal => {
+            this.Modal = Modal;
+          }}
+          id="modal1"
+          className="modal"
+        >
+          {/* If you want Bottom Sheet Modal then add 
+                        bottom-sheet class to the "modal" div
+                        If you want Fixed Footer Modal then add
+                        modal-fixed-footer to the "modal" div*/}
+          <div className="modal-content">
+            <h4>New Topic</h4>
+            {/* Name of the user */}
+            <Input inputType={'text'}
+            title= {'Username'} 
+            name= {'username'}
+            value={this.state.newPost.name} 
+            placeholder = {'Enter your name'}
+            handleChange = {this.handleInput}/> 
+              {/* Name of the topic */}
+            <Input inputType={'text'}
+                  title= {'Topic'} 
+                  name= {'name'}
+                  value={this.state.newPost.name} 
+                  placeholder = {'Enter your topic'}
+                  handleChange = {this.handleInput}/> 
+                
+          {/* Discussion*/}
+          <TextArea
+            title={'What would you like to discuss?'}
+            rows={10}
+            value={this.state.newPost.text}
+            name={'Discussion'}
+            handleChange={this.handleTextArea}
+            placeholder={'type away!'} />
+
+          <div className="modal-footer">
+            <button action={this.handleSubmitForm} type={'primary'} title={'submit'} className="modal-close waves-effect waves-green btn-flat">
+              Post
+            </button>
+            <button action={this.handleClearForm} type={'secondary'} title={'clear'}  className="modal-close waves-effect waves-red btn-flat">
+              Discard
+            </button>
+          </div>
+        </div>
+      </div>
+      </div>
+
+    );
+  }
+}
+
+export default Modal;
