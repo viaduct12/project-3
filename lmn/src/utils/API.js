a82c78485ff4aef00faf8a0417700f4a0020e9c0
Lawrence Fiesta <lfiesta86@gmail.com>
2019-08-12 19:20:28 -0700

    pulling meghans work

--- lmn/src/utils/API.js
+++ lmn/src/utils/API.js
@@ -1,15 +1,27 @@
 import axios from "axios";
+import Auth from "./Auth";
 require('dotenv').config();
+
+const headers = () => {
+  const options = {};
+  // if authenticated create Authorization header to add to api calls
+  if (Auth.isAuthenticated()) {
+    options["headers"] = {
+      "Authorization": `Bearer ${Auth.getToken()}`
+    }
+  }
+  return options;
+}
 
 export default {
   scrapeArticles: function(query) {
     console.log("scrape test");
-    return axios.get("https://lmn-news-api.herokuapp.com/scrape/" + query);
-    // return axios.get("http://localhost:3000/scrape/" + query);
+    // return axios.get("https://lmn-news-api.herokuapp.com/scrape/" + query);
+    return axios.get("http://localhost:3000/api/article/scrape/" + query, headers());
   },
   getArticles: function(query) {
-    return axios.get("https://lmn-news-api.herokuapp.com/api/" + query)
-    // return axios.get("http://localhost:3000/api/" + query);
+    // return axios.get("https://lmn-news-api.herokuapp.com/api/" + query)
+    return axios.get("http://localhost:3000/api/article/get/" + query, headers());
   },
 
   searchPodcast: function(query) {
@@ -30,7 +42,7 @@ export default {
     // return axios.post("https://lmn-news-api.herokuapp.com/api/signup", { data: {
 
     //test link
-    return axios.post("http://localhost:3000/api/signup", { data: {
+    return axios.post("http://localhost:3000/api/auth/signup", { data: {
       "firstName": userInfo.firstName,
       "lastName": userInfo.lastName,
       "username": userInfo.username,
@@ -40,8 +52,8 @@ export default {
   },
   loginUser: function(userCredentials) {
     console.log(userCredentials, "inside log in request");
-    return axios.post("https://lmn-news-api.herokuapp.com/api/login", { data: {
-    // return axios.post("http://localhost:3000/api/login", { data: {
+    // return axios.post("https://lmn-news-api.herokuapp.com/api/login", { data: {
+    return axios.post("http://localhost:3000/api/auth/login", { data: {
       "email": userCredentials.email,
       "password": userCredentials.password
     }})
