369153e792d4c8f669ef2bd0450a73fa66469bb9
ninateng <niinateng@gmail.com>
2019-08-14 09:50:15 -0700

    updating populated page

--- lmn/src/components/Youtube/SearchBar.js
+++ lmn/src/components/Youtube/SearchBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Searchbar extends React.Component {
     state = {
-        term: 'Default text'
+        term: ''
     };
     handleChange = (event) => {
         this.setState({
