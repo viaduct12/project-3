3839252f73a0e6a4c69043ca71483406fb6e3e27
Lawrence Fiesta <lfiesta86@gmail.com>
2019-08-13 21:44:32 -0700

    fixed the nav bar

--- lmn/src/components/Navbar/index.js
+++ lmn/src/components/Navbar/index.js
@@ -5,8 +5,8 @@ import "./style.css";
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/">
+    <nav className="navbar">
+      <Link className="brand-logo right" to="/">
         Learn More News
       </Link>
       <div>
