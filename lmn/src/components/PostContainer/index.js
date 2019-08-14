27d7d5aa42e4ca7e198baee614f5f6a4ffd32fae
Meghan Mullally <ms.mullally27@gmail.com>
2019-08-13 23:39:51 -0700

    connected SingleDiscussion page with single Post, also connecting the comment sections, changed styling

--- lmn/src/components/PostContainer/index.js
+++ lmn/src/components/PostContainer/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import './style.css';
 
 export default function PostContainer(props) {
@@ -9,7 +10,9 @@ export default function PostContainer(props) {
     //   <h2>Discussions</h2>
     // <div className="divider"></div>
       <div className="section">
+        <Link to="/singleDiscussion/:category">
         <h5>{props.topic}</h5>
+        </Link>
         <h6>{props.username}</h6>
         <p>{props.description}</p>
         <p>{props.createdAt}</p>
