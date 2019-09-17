a1477ee49afbfdfb19c67b6ef1a926b949adc42f
ninateng <niinateng@gmail.com>
2019-09-17 16:00:08 -0700

    fixed me  page

--- lmn/src/pages/Me/index.js
+++ lmn/src/pages/Me/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import './style.css';
 class Me extends React.Component {
 
@@ -7,7 +8,32 @@ class Me extends React.Component {
         return (
             <div id="containerMe">
 
-        <div id="leftBlock">
+<div id="leftBlock">
+
+<div className="leftContainer">
+<h1 className="hello">Hello </h1>
+<br />
+<div id="userInfoBlock">
+    <p className="blurbBlock">
+        
+                <br/>
+                <br/>
+                <p id="userNameSection">username: </p>
+                <p id="emailSection">email: </p>
+                <br/>
+                </p>
+
+                <p id="explore">Explore your favorites -> </p>
+         
+    <br />
+</div>
+</div>
+<br />
+<br />
+
+</div>
+
+        {/* <div id="leftBlock">
             <h1>Hello ______ </h1>
             <br/>
             <br/>
@@ -25,36 +51,38 @@ class Me extends React.Component {
             <br/>
             <br/>
     
-        </div>
+        </div> */}
+
+        
 
         <div id="rightBlock">
             <h1 id="titleFav">Favorites</h1>
             <br/>
             <br/>
 
-            {/* -------------------ROW 1---------------- */}
-            <div id="row1">
-                <div class="column" id="row1L">
-                    <h1>movements</h1>
-                </div>
-                    
-                <div class="column" id="row1R">
-                    <h1>tech</h1>
-                </div>
-            </div>
-            {/* -------------------ROW 1---------------- */}
+                    {/* -------------------ROW 1---------------- */}
+                    <div className="row" id="row1">
+                        <div className="column" id="row1L">
+                            <Link to="/populate/politics"><h2 className="topicName">politics</h2></Link>
+                        </div>
 
-            {/* -------------------ROW 2---------------- */}
-            <div id="row2">
-                <div class="column" id="row2L">
-                    <h1>politics</h1>
-                </div>
-                    
-                <div class="column" id="row2R">
-                    <h1>anime & gaming</h1>
-                </div>
-            </div>
-            {/* -------------------ROW 2---------------- */}
+                        <div className="column" id="row1R">
+                            <Link to="/populate/technology"><h2 className="topicName">tech</h2></Link>
+                        </div>
+                    </div>
+                    {/* -------------------ROW 1---------------- */}
+
+                    {/* -------------------ROW 2---------------- */}
+                    <div className="row" id="row2">
+                        <div className="column" id="row2L">
+                            <Link to="/populate/sports"><h2 className="topicName">sports</h2></Link>
+                        </div>
+
+                        <div className="column" id="row2R">
+                            <Link to="/populate/media"><h2 className="topicName">media</h2></Link>
+                        </div>
+                    </div>
+                    {/* -------------------ROW 2---------------- */}
 
         </div>
         
