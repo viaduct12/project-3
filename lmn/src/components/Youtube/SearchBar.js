016c6bd82a0871a9118f016da25c32f8f8e5e8c6
ninateng <niinateng@gmail.com>
2019-09-17 14:57:43 -0700

    adjustments to populated page

--- lmn/src/components/Youtube/SearchBar.js
+++ lmn/src/components/Youtube/SearchBar.js
@@ -19,7 +19,7 @@ class Searchbar extends React.Component {
 
     render() {
         return (
-            <div className='search-bar ui segment'>
+            <div id="searchBox" className='search-bar ui segment'>
                 <form onSubmit={this.handleSubmit} className='ui form'>
                     <div className='field'>
                         <label id="video-search" htmlFor="video-search">Video Search</label>
--- lmn/src/components/Youtube/VideoDetail.js
+++ lmn/src/components/Youtube/VideoDetail.js
@@ -3,13 +3,13 @@ import './style/video.css'
 
 const VideoDetail = ({video}) => {
     if (!video) {
-        return <div id="loading-div">Loading ...</div>;
+        return <div id="loading-div"></div>;
     }
 
     const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
     console.log(typeof(video));
     return (
-        <div>
+        <div id="videoActualContainer">
             <div className='ui embed' id="videoContainer">
                 <iframe src={videoSrc} id="embed" allowFullScreen title='Video player'/>
             </div>
